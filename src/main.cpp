a376b6ce498f29c6bb04e104cda384347daf8c4a
stekap000 <stekap000@gmail.com>
2024-04-28 17:24:50 +0200

    stbi working

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
+#include <cstdint>
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
 
 int main() {
-	std::cout << "Get ASCIIed!!!" << std::endl;
+	int width, height, num_channels;
+	uint32_t *data = (uint32_t *)stbi_load("test.jpg", &width, &height, &num_channels, 0);
+
+	std::cout << "Width: " << width << std::endl;
+	std::cout << "Height: " << height << std::endl;
+	std::cout << "Channels: " << num_channels << std::endl;
+
+	stbi_image_free(data);
 	
 	return 0;
 }
